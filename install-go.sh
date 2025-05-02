0eb4b80f1c0fd77f6366aa21bd824f6f44636164
Etienne Charlier <etienne@phi8.ovh>
2025-05-02 14:43:38 +0200

    Prepare upgrade to noble

--- install-go.sh
+++ install-go.sh
@@ -2,5 +2,5 @@
 
 wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
 tar xvzf go1.24.2.linux-amd64.tar.gz
-sudo mv go /etc/local
+sudo mv go /usr/local
 echo 'export PATH=$PATH:/usr/local/go/bin' >>$HOME/.bashrc

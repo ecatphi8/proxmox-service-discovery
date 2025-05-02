13a6cfae0f0166a9f72976e1d2b05be16038381e
Etienne Charlier <etienne@phi8.ovh>
2025-05-02 15:50:47 +0200

    Prepare upgrade to noble

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ sudo install --owner=root --group=root --mode=644 \
 #5. Configure system resolver
 sudo mkdir -p /etc/systemd/resolved.conf.d
 cat <<'EOF' >/tmp/resolved.conf 
+[Resolve]
 DNS=172.19.1.3 172.19.1.4
 DNSStubListener=no
 EOF
